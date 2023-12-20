65a518744125761e401ab4bf998db643850d4c4d
willtruong236 <willtruong236@gmail.com>
2023-12-21 06:27:22 +1030

    Add project files.

---
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,25 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === 'success') {
+        toastr.success(message, 'Operation Successful', { timeOut: 5000 });
+    }
+    if (type === 'error') {
+        toastr.error(message, 'Operation Failed', { timeOut: 5000 });
+    }
+}
+
+window.ShowSweetAlert = (type, message) => {
+    if (type === 'success') {
+        Swal.fire({ icon: "success", title: "Success Notification!", text: message });
+    }
+    if (type === 'error') {
+        Swal.fire({ icon: "error", title: "Error Notification!", text: message })
+    }
+}
+
+function ShowDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('show');
+}
+
+function HideDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('hide');
+}
